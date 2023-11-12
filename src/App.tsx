23a390e42716c68b075d33b704d93be3f17909d8
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-11-12 23:16:05 +0900

    feat: 文字を真ん中にする

--- src/App.tsx
+++ src/App.tsx
@@ -34,26 +34,30 @@ function App() {
             </header>
             <div className="splash">
                 <div className="background">
+
                     <img alt="" aria-hidden="true" data-uia="nmhp-card-hero+background+image"
                          src="https://assets.nflxext.com/ffe/siteui/vlv3/77d35039-751f-4c3e-9c8d-1240c1ca6188/65f1964b-8406-4fed-ac0e-9adbe0e4d994/JP-en-20231106-popsignuptwoweeks-perspective_alpha_website_small.jpg"
                          srcSet="https://assets.nflxext.com/ffe/siteui/vlv3/77d35039-751f-4c3e-9c8d-1240c1ca6188/65f1964b-8406-4fed-ac0e-9adbe0e4d994/JP-en-20231106-popsignuptwoweeks-perspective_alpha_website_small.jpg 1000w, https://assets.nflxext.com/ffe/siteui/vlv3/77d35039-751f-4c3e-9c8d-1240c1ca6188/65f1964b-8406-4fed-ac0e-9adbe0e4d994/JP-en-20231106-popsignuptwoweeks-perspective_alpha_website_medium.jpg 1500w, https://assets.nflxext.com/ffe/siteui/vlv3/77d35039-751f-4c3e-9c8d-1240c1ca6188/65f1964b-8406-4fed-ac0e-9adbe0e4d994/JP-en-20231106-popsignuptwoweeks-perspective_alpha_website_large.jpg 1800w"
                          className="default-ltr-cache-ve3cf8 eae08lb0"/>
                     <div className="gradient"></div>
+
                 </div>
-                <div>
-                    <h1>
-                        Enjoy big movies, hit series and more from JPY790.
-                    </h1>
-                    <h2>
-                        Join today. Cancel anytime.
-                    </h2>
-                    <h3>
-                        Ready to watch? Enter your email to create or restart your membership.
-                    </h3>
-                    <form placeholder="Email Address">
-                        <input type="email"></input>
-                    </form>
-                    <button>Get Started &gt;</button>
+                <div className="content">
+                    <div>
+                        <h1>
+                            Enjoy big movies, hit series and more from JPY790.
+                        </h1>
+                        <h2>
+                            Join today. Cancel anytime.
+                        </h2>
+                        <h3>
+                            Ready to watch? Enter your email to create or restart your membership.
+                        </h3>
+                        <form>
+                            <input type="email" placeholder="Email Address"></input>
+                            <button>Get Started &gt;</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </>
