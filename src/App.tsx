eeef11aa5179a4036df2120e693833b3f82987e3
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-11-13 23:43:18 +0900

    feat: Emailインプットフォーム＆ボタン

--- src/App.tsx
+++ src/App.tsx
@@ -54,8 +54,13 @@ function App() {
                             Ready to watch? Enter your email to create or restart your membership.
                         </h3>
                         <form>
-                            <input type="email" placeholder="Email Address"></input>
-                            <button>Get Started &gt;</button>
+                            <div>
+                                <input type="email"/>
+                                <span>Email Address</span>
+                            </div>
+                            <div>
+                                <button className="getStartedButton">Get Started &gt;</button>
+                            </div>
                         </form>
                     </div>
                 </div>
