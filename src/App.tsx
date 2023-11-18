fd66b8cb28e44d22cf5140f90d639ce38cfba50b
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-11-18 23:52:09 +0900

    feat: 質問箇所の実装

--- src/App.tsx
+++ src/App.tsx
@@ -127,6 +127,28 @@ function App() {
                     <p>Watch on a plane, train, or submarine...</p>
                 </div>
             </section>
+            <div className="faq">
+                <h1>Frequently Asked Questions</h1>
+                <div className="questions">
+                    <div className="question">
+                        <div className="questionHeader">
+                            What is Netflix ?
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                                 stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"/>
+                            </svg>
+                        </div>
+                        <div className="questionAnswer">
+                            Netflix is a streaming service that offers a wide variety of award-winning TV shows,
+                            movies, anime, documentaries, and more on thousands of internet-connected devices.
+
+                            You can watch as much as you want, whenever you want – all for one low monthly price.
+                            There's always something new to discover and new TV shows and movies are added every
+                            week!
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
